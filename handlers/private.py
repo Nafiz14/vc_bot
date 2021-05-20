a5593c37c8e8e637707a55367d1a0b7258a5bd17
Nafiz14 <83584219+Nafiz14@users.noreply.github.com>
2021-05-20 19:12:12 +0600

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -21,10 +21,10 @@ async def start(_, message: Message):
         [
             [
                 InlineKeyboardButton(
-                    "Group 💬", url="https://t.me/Its_Fuckin_Hell"
+                    "Group 💬", url="https://t.me/bothelp1"
                 ),
                 InlineKeyboardButton(
-                    "Channel 📣", url="https://t.me/The_HellBot"
+                    "Channel 📣", url="https://t.me/oploots"
                 )
             ]
         ]
@@ -51,7 +51,7 @@ Happy Streaming 😉
                         "Repo 📑", url="https://github.com/TheVaders/vc_bot"
                     ),
                     InlineKeyboardButton(
-                        "Channel 📣", url="https://t.me/The_HellBot"
+                        "Channel 📣", url="https://t.me/oploots"
                     ),
                     InlineKeyboardButton (
                         "Tutorial 🎬", url="https://youtu.be/XaH7VHURBVg"
